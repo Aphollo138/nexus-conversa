feea128aa8d69030d3055742c57ab80a9666ef60
Luan <luancardoso678910@gmail.com>
2026-03-14 11:22:12 -0300

    logos

--- src/components/PaposLogo.tsx
+++ src/components/PaposLogo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function NexusLogo({ className = "w-8 h-8" }: { className?: string }) {
+export default function PaposLogo({ className = "w-8 h-8" }: { className?: string }) {
   return (
     <div className={`flex items-center justify-center bg-white rounded-xl shadow-[0_0_15px_rgba(255,255,255,0.3)] overflow-hidden p-1 ${className}`}>
       <img 
