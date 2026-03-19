2f59e1b3a56ab42ced899b87d85f1eca1ab4bc01
Luan <luancardoso678910@gmail.com>
2026-03-18 22:27:50 -0300

    feat: setup android pwa native

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.net.papo.app',
+  appName: 'papos',
+  webDir: 'dist'
+};
+
+export default config;
